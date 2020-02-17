21950dd4bbc86f433d0da9a19bb080081e881d65
Aidan Shaughnessy <aidanshaughnessy@Aidans-MacBook-Pro.local>
2020-02-16 21:42:28 -0800

    mike index major

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
+$servername = "my-project-mikeapp:us-west2:mikedb";
+$username = "root";
+$password = "UnitySongRoot1";
+$dbname = "people_data";
 
-echo "hello world! ffffudge s yaml  x334";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT entryID, guestName, content FROM entries";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["entryID"]. " - Name: " . $row["guestName"]. " " . $row["content"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
